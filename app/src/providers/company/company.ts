98a1174571b86084e57a80d08504222b8914be0c
Kiyonori Matsumoto <kiyonori.matsumoto@gmail.com>
2017-11-24 03:09:09 +0900

    file途中

--- app/src/providers/company/company.ts
+++ app/src/providers/company/company.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { ReplaySubject, Observable, ConnectableObservable } from 'rxjs';
+import { ReplaySubject, Observable, ConnectableObservable, Subject } from 'rxjs';
 import { File } from '@ionic-native/file';
 import * as firebase from 'firebase';
 
@@ -16,6 +16,8 @@ import * as firebase from 'firebase';
 export class CompanyProvider {
 
   private obs: Observable<firebase.firestore.QuerySnapshot>;
+  private companiesSubject: Subject<any> = new Subject()
+  public companies$: Observable<any> = this.companiesSubject.publishReplay(1).refCount();
 
   constructor(
     private afs: AngularFirestore,
@@ -25,22 +27,48 @@ export class CompanyProvider {
     const filename = 'companies.json';
     file.checkFile(file.dataDirectory, filename)
     .then(result => {
-      if (result) {
-
+      console.log(`result = ${result}`)
+      if (!result) {
+        this.afs.collection('companies').ref.get()
+        .then(query => {
+          console.log(`length = ${query.docs.length}`)
+          const companies = query.docs.map(e => Object.assign({}, e.data(), {id: e.id})) || [];
+          this.file.writeFile(file.dataDirectory, filename, JSON.stringify(companies), {replace: true})
+          .then(() => this.companiesSubject.next(companies));
+        })
       } else {
-        this.obs = Observable.fromPromise(this.afs.collection('companies').ref.get()).publishReplay(1).refCount();
+        this.file.readAsText(file.dataDirectory, filename)
+        .then(text => JSON.parse(text))
+        .then(companies => this.companiesSubject.next(companies));
       }
+      // if (result) {
+        
+      // } else {
+      //   this.obs = Observable.fromPromise(this.afs.collection('companies').ref.get()).publishReplay(1).refCount();
+      // }
+    })
+    .catch(err => {
+      console.log(err.message || err.code || err)
+
+      this.afs.collection('companies').ref.get()
+      .then(query => {
+        console.log(`length = ${query.docs.length}`)
+        const companies = query.docs.map(e => Object.assign({}, e.data(), {id: e.id})) || [];
+        this.file.writeFile(file.dataDirectory, filename, JSON.stringify(companies), {replace: true})
+        .then(() => this.companiesSubject.next(companies));
+      })
     })
   }
 
   public all() {
-    return this.obs.map(e => e.docs);
+    // return this.obs.map(e => e.docs);
+    return this.companies$;
   }
 
   public byCode(code: string) {
     return this.all()
     .map(e => e.find(_e => _e.id === code))
-    .map(e => e ? e.data(): {})
+    // .map(e => e ? e.data(): {})
     .do(e=>console.log(e))
   }
 
