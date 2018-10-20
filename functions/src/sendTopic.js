f927018a65334097c1b348379553f270f3defd6e
Kiyonori Matsumoto <kiyonori.matsumoto@gmail.com>
2018-10-20 22:41:45 +0900

    FirebaseのTopicの送信内容を変更

--- functions/src/sendTopic.js
+++ functions/src/sendTopic.js
@@ -2,14 +2,14 @@ const admin = require('firebase-admin');
 
 const sendTopic = (snapshot, context) => {
   const data = snapshot.data();
-  console.log(data.title, data.code, data.noSend);
+  console.log(data.title, data.code, data.noSend, data.company);
   if (data.noSend) return true;
   return admin.messaging().sendToTopic(`code_${data.code}`, {
     notification: {
-      body: `(${data.code}) ${data.title}`,
+      body: `${data.title}`,
       tag: `code_${data.code}`,
       color: '#006064',
-      title: '新しい開示情報',
+      title: `新しい開示情報 ${data.company}(${data.code})`,
       // click_action: 'FCM_PLUGIN_ACTIVITY', // remove here when cordova-plugin-firebase 
     }, data: {
       code: `${data.code}`,
