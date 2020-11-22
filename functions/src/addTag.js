8af615d3dd93f59a9627feace1d8ef2f7c1c0322
kiyonori.matsumoto <kiyonori.matsumoto@sansan.com>
2020-11-22 21:04:20 +0900

    短信をタグに追加

--- functions/src/addTag.js
+++ functions/src/addTag.js
@@ -1,13 +1,22 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
-const TAGS = ['株主優待', '決算', '配当', '業績予想', '日々の開示事項', '自己株式', '新株']
+const TAGS = [
+  "株主優待",
+  "決算",
+  "配当",
+  "業績予想",
+  "日々の開示事項",
+  "自己株式",
+  "新株",
+  "短信",
+];
 
 const addTag = (doc, context) => {
   const data = doc.data();
-  
-  const tags = TAGS.filter(e => data.title.match(e))
-  .reduce((a, e) => {
-    a[e] = true; return a;
+
+  const tags = TAGS.filter((e) => data.title.match(e)).reduce((a, e) => {
+    a[e] = true;
+    return a;
   }, {});
 
   console.log(JSON.stringify(tags));
