2438a14ca18a30d72a97fd4c9e837f7724cc49ba
kiyonori.matsumoto <kiyonori.matsumoto@sansan.com>
2024-04-21 10:47:47 +0900

    remove ionic version

--- app/build_android.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-rm disclosure.apk -f
-ionic cordova build android --prod --release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/my-release-key.jks platforms/android/build/outputs/apk/release/android-release-unsigned.apk my-release
-~/Android/Sdk/build-tools/28.0.2/zipalign -v 4 platforms/android/build/outputs/apk/release/android-release-unsigned.apk disclosure.apk
--- app/src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-// import { MyPdfViewerComponent } from './my-pdf-viewer/my-pdf-viewer';
-import { IonicModule } from 'ionic-angular';
-@NgModule({
-	declarations: [],
-	imports: [IonicModule],
-	exports: []
-})
-export class ComponentsModule {}
